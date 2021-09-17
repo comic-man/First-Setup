05ecadcebeefcd6e781d49f61a1948dd6daeb139
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-09-16 23:12:13 -0500

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'David is trying really hard';
+  name = 'David is trying really hard';
 }
